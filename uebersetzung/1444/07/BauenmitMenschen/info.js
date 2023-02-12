34722e39aa09121da9a99d413c3276074d3330c1
OmerHanRustem <omer.han.1431@gmail.com>
2023-02-13 00:04:52 +0200

    Update

--- uebersetzung/1444/07/BauenmitMenschen/info.js
+++ uebersetzung/1444/07/BauenmitMenschen/info.js
@@ -1,66 +1,25 @@
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-let order = 2023-01-04,
-pagePrice = 200,
-setting = 100,
-words = 7846,
-thisPartWords = 3928,
-pricing = 2023-01-09,
-price = 5400,
-dis = 0.15,
-contract = new Date ('2023-01-11T23:32:00'),
-deadline = 14,
-today = new Date (),
-advance = 800,
-totalPrice = (words / 250) * pagePrice,
-discounted = totalPrice - (totalPrice * dis);
+const download = document.getElementsByClassName('mybtn');
+var clickCounter = 0;
 
-function rounding(num) {
-    let rounded = 0,
-    remainder = num % 100;
-    if (remainder >= 50) {
-        rounded = num - remainder + 100;
-    } else {
-        rounded = num - remainder;
+download[0].addEventListener("click", joke, false);
+
+function joke(event) {
+  event.preventDefault();
+  const joke = document.getElementById("joke"),
+  joke2 = document.getElementById("joke2");
+
+    clickCounter++;
+    if (clickCounter == 1) {
+      joke.style.visibility = "visible";
+    } else if (clickCounter == 2) {
+      joke2.style.visibility = "visible";
+      joke.style.visibility = "hidden";
+    } else if (clickCounter == 3) {
+      joke2.style.visibility = "hidden";
+      window.open('BauenMitMenschen.docx', '_blank');
+      clickCounter = 0;
     }
-    return rounded;
-};
 
-document.write('<hr>');
+}
 
-    document.write(
-    
-    '<div class="other" style="direction:rtl; color:lightgray; margin:40px; font-family:monospace;">'
-+	'<ul>'
-+	"<li>في هذه الصفحة ترجمة: " + thisPartWords + " كلمة من أصل " + words + " كلمة (" + Math.ceil((thisPartWords * 100) / words) + "%) </li>"
-+	"<li> سعر الصفحة: " + pagePrice + "ج بالإضافة إلى خصم " + (dis * 100) + "%</li>"
-+	`<li> السعر الإجمالي: ${rounding(totalPrice + setting)}ج، وبعد الخصم: ${rounding(discounted) + setting}ج </li>`
-+	"<li> تعوقد في: " + contract.toLocaleDateString('ar-EG', options) + "، الساعة: " + contract.toLocaleTimeString('ar-EG') + "</li>"
-+	`<li> على أن يُسلَّم النصف الأول خلال ${deadline} يوما بحد أقصى يوم: ` + addDays(contract, deadline).toLocaleDateString('ar-EG', options) + "</li>"
-+	`<li> تاريخ اليوم: ${today.toLocaleDateString('ar-EG', options)}</li>`
-+	`<li> العربون المدفوع: ${advance}ج </li>`
-+	`<li> المتبقي: ${rounding(discounted - advance) + setting}ج</li>`
-+ 	'</div>'
-+	'</ul>'
-    );
-
-document.write('<hr>');
-
-function addDays(date, days) {
-var result = new Date(date);
-result.setDate(result.getDate() + days);
-return result;
-};
-
-// document.write(
-// 	'<div style="direction:ltr; color:lightgray; margin:40px; font-family:monospace;">' +
-// 	"<h2 style='color:lightgray; margin:10px;'>Translation Info</h2>"
-// +	"<li>These are: " + thisPartWords + " out of " + words + " words (" + Math.round((thisPartWords * 100) / words) + "%) </li>"
-// +	"<li> Page Price is: " + pagePrice + " EGP with discount of " + (dis * 100) + "%</li>"
-// +	`<li> Total Price is: ${rounding(totalPrice + setting)} EGP, after DISCOUNT: ${rounding(discounted) + setting} EGP </li>`
-// +	"<li> Contracted in: " + contract.toLocaleDateString('en-UK', options) + "</li>"
-// +	`<li> To be delivered in ${deadline} days on: ` + addDays(contract, deadline).toLocaleDateString('en-UK', options) + "</li>"
-// +	`<li> Today is: ${today.toLocaleDateString('en-UK', options)}</li>`
-// +	`<li> Advance paid: ${advance} EGP </li>`
-// + 	'</div>'
-// 	);
-    
+console.log(clickCounter);
