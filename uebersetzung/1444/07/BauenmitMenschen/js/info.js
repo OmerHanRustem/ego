9a47be2bc86b9b24f3d3f5130ac68514a50c787c
OmerHanRustem <omer.han.1431@gmail.com>
2023-02-13 00:16:19 +0200

    info.js

---
+++ uebersetzung/1444/07/BauenmitMenschen/js/info.js
@@ -0,0 +1,23 @@
+const download = document.getElementsByClassName("mybtn");
+var clickCounter = 0;
+
+download[0].addEventListener("click", joke, false);
+
+function joke(event) {
+  event.preventDefault();
+  let joke = document.getElementById("joke"),
+  joke2 = document.getElementById("joke2");
+  clickCounter++;
+  if (clickCounter == 1) {
+    joke.style.visibility = "visible";
+  } else if (clickCounter == 2) {
+    joke2.style.visibility = "visible";
+    joke.style.visibility = "hidden";
+  } else if (clickCounter == 3) {
+    joke2.style.visibility = "hidden";
+    window.open('BauenMitMenschen.docx', '_blank');
+    clickCounter = 0;
+  }
+};
+
+console.log(clickCounter);
